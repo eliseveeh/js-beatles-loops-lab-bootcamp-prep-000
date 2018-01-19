3d19615a98de921f0216eba96f2733e67f281626
Elizabeth Veeh <elise.veeh@gmail.com>
2018-01-19 19:09:31 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+function theBeatlesPlay(musicians, instruments){
+  var array = [];
+  for (var x = 0, x < musicians.length; x++){
+    array.push("${musicians[x]} plays ${instruments[x]}.")
+  }
+  return array;
+}
